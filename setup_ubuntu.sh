80ff5866fc4e457b08eeb10f273e08f1708f6c1a
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2022-08-27 16:49:24 -0400

    started separate ubuntu setup

---
+++ setup_ubuntu.sh
@@ -0,0 +1,14 @@
+sudo apt-get install gnome-tweaks
+sudo apt-get install git
+sudo apt-get install htop vim-nox curl ack tree
+sudo apt-get install esl-erlang
+sudo apt-get install cryptsetup
+sudo apt-get install gparted
+sudo apt-get install neofetch
+sudo apt-get install ncal
+sudo apt-get install mesa-utils
+sudo apt-get install glxinfo
+sudo apt-get install gufw
+sudo apt-get install tcpdump
+sudo apt-get install net-tools
+sudo apt-get install ripgrep
